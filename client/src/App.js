be35fddaa17e1acc727d52ff9600729d98e572c2
Horatiu Andrei <horatiu.andrei@gmail.com>
2025-04-03 16:29:14 +0530

    Client: Updated text

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
               target="_blank"
               rel="noopener noreferrer"
           >
-            Learn React Now! 💻
+            Learn React Now! 💻⚛️
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
