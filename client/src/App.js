7c21f99365d81589423ba7f0d89b6bdd7c738d7e
Horatiu Andrei <horatiu.andrei@gmail.com>
2025-04-04 12:52:51 +0530

    Skaffold support for development

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
               target="_blank"
               rel="noopener noreferrer"
           >
-            Learn React Now! 💻⚛️
+            Learn React Now! 💻⚛️⚛⚛⚛
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
